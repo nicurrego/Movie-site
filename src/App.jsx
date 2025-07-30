29351ca73c5b4a8d80a03afc2afb86a31864897f
Nicolas Urrego <nicurrego@gmail.com>
2025-07-30 22:38:29 +0900

    feat(src): added a NavBar component that wraps all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import Home from './components/pages/Home'
 import Favorites from './components/pages/Favorites'
 import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <main className='main-content'>
+    <div>
+      <NavBar />
+      <main className='main-content'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favorites' element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   )
 }
 
