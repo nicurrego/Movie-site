fb346552a203eef1eac3a7fc623b51f94803a957
Nicolas Urrego <nicurrego@gmail.com>
2025-07-30 20:09:04 +0900

    feat(src): handle page routing - using BrowserRouter created the home and favorites pages route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Home from './components/pages/Home'
+import Favorites from './components/pages/Favorites'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/components/pages/Favorites.jsx
@@ -0,0 +1,7 @@
+function Favorite() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites list</p>
+    </div>
+}
+export default Favorite;
